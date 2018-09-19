dad8b5bcb80cf9b9b427c94487852e6f0841b864
William Jiang <wj537@nyu.edu>
2018-09-19 10:53:28 -0400

    Added gitignore and first commit of volleyball game

---
+++ Volleyball/Assets/player1control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player1control : MonoBehaviour {
+
+    public Rigidbody2D left;
+    public Rigidbody2D right;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("d"))
+        {
+            this.transform.position += new Vector3(0.03f, 0);
+        }
+        else if (Input.GetKey("a"))
+        {
+            this.transform.position += new Vector3(-0.03f, 0);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            //left.AddForce(left.transform.up * 100.0f);
+            //right.AddForce(right.transform.up * 100.0f);
+            left.AddForce(new Vector2(0f, 425f));
+            right.AddForce(new Vector2(0f, 425f));
+        }
+    }
+}
---
+++ Volleyball/Assets/player2control.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player2control : MonoBehaviour {
+
+    public Rigidbody2D left;
+    public Rigidbody2D right;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            this.transform.position += new Vector3(0.03f, 0);
+        }
+        else if (Input.GetKey("left"))
+        {
+            this.transform.position += new Vector3(-0.03f, 0);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            left.AddForce(left.transform.up * 10.0f);
+            right.AddForce(right.transform.up * 10.0f);
+        }
+    }
+}
